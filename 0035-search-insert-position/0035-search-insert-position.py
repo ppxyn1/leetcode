68ca197d1d4760899a6daf7c79e701e2ff136650
yjna08 <120540450+yjna08@users.noreply.github.com>
2023-01-23 21:11:26 +0900

    Time: 57 ms (54.05%), Space: 14.8 MB (34.89%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -3,17 +3,21 @@ class Solution:
         nums.sort()
         results = 1e9
         idx = []
-        # case1: nums has target 
         for i in range(len(nums)):
+            # case1: nums doesn`t have a target and range in nums
             if nums[i] > target:
                 idx.append(i)
+            # case2: nums has target 
             elif nums[i] == target: 
                 results= i
-        # case2: nums doesn`t have a target
-        print(idx)
+        # target > max OR target < min among case1       
         if results == 1e9 and target>max(nums):
             return len(nums)
         elif results == 1e9 and target<max(nums):
             return min(idx)  
+        # case 2
         else:
             return results
+        
+ 
+        
