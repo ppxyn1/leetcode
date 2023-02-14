26d1614fc70c71b9c1153dd11682fb3960584a70
YOUNGJIN NA <120540450+yjna08@users.noreply.github.com>
2023-02-15 01:24:04 +0900

    Time: 34 ms (74.68%), Space: 13.8 MB (63.05%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,6 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        binA = int(a,2)
+        binA = int(a,2) #이진수를 변환
         binB = int(b,2)
         
         print(binA, binB)
