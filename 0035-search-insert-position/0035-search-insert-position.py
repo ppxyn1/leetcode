a17e0d360188a26184ec1c3fc11b9940c4f37531
YOUNGJIN NA <120540450+yjna08@users.noreply.github.com>
2023-02-23 12:32:41 +0900

    Time: 51 ms (70.14%), Space: 14.7 MB (30.80%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,7 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         nums.sort()
-        start = 0;
+        start = 0
         end = len(nums)-1
         
         while(start <= end):
@@ -16,24 +16,3 @@ class Solution:
         
         
         
-        
-        ''' solution1
-        nums.sort()
-        results = 1e9
-        idx = []
-        for i in range(len(nums)):
-            # case1: nums doesn`t have a target and range in nums
-            if nums[i] > target:
-                idx.append(i)
-            # case2: nums has target 
-            elif nums[i] == target: 
-                results= i
-        # target > max OR target < min among case1       
-        if results == 1e9 and target>max(nums):
-            return len(nums)
-        elif results == 1e9 and target<max(nums):
-            return min(idx)  
-        # case 2
-        else:
-            return results
-        '''
